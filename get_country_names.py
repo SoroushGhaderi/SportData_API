6d5e1408b87200335a00ebbe47b23ef51b6c037d
Soroush <ghaderi.soroush1995@gmail.com>
2021-04-03 18:03:41 +0430

    country json data

---
+++ get_country_names.py
@@ -0,0 +1,31 @@
+import config
+
+import os
+import json
+
+import requests
+
+
+def url_request():
+    headers = {"apikey": config.apikey}
+    params = (("continent", "Europe"),)
+    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/countries', headers=headers, params=params)
+    return response.json()
+
+
+def save_object(file_name, saved_object, writing_method):
+    saved_object = json.dumps(saved_object, indent=4, ensure_ascii=False)
+    with open(file_name, writing_method) as handler:
+        handler.writelines(saved_object)
+
+
+def main():
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    response_json = url_request()
+    save_object("data/country_names.json", response_json, "w")
+
+
+if __name__ == "__main__":
+    main()
